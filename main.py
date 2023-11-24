e9d1302caa3e3fe6e0af88ad3b8f46e4c091eb00
elxp101t3r <eliotx10101@outlook.com>
2023-11-24 16:46:36 +0200

    Completed!

--- main.py
+++ main.py
@@ -1,42 +1,36 @@
 from turtle import Turtle, Screen
-from random import choice
-
+from random import randint
+race_mode = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title='Bet!', prompt='which turtle is going to win this race?("red", "green", "orange" or "purple")')
 colors = ["red", "green", "orange", "purple"]
+y_pos = [-40, -20, 0, 20]
 turtles = []
-for i in range(len(colors)):
-    turtles.append(Turtle(shape='turtle'))
-for i in range(len(turtles)):
-    if i == 0:
-        t1 = turtles[i]
-    elif i == 1:
-        t2 = turtles[i]
-    elif i == 2:
-        t3 = turtles[i]
-    elif i == 3:
-        t4 = turtles[i]
 
-c1 = choice(colors)
-colors.remove(c1)
-c2 = choice(colors)
-colors.remove(c2)
-c3 = choice(colors)
-colors.remove(c3)
-c4 = choice(colors)
-colors.remove(c4)
-t1.color(c1)
-t1.penup()
-t1.goto(x=-225, y=-100)
-t2.color(c2)
-t2.penup()
-t2.goto(x=-225, y=-50)
-t3.color(c3)
-t3.penup()
-t3.goto(x=-225, y=0)
-t4.color(c4)
-t4.penup()
-t4.goto(x=-225, y=50)
+for i in range(len(colors)):
+    n_turtle = Turtle(shape='turtle')
+    n_turtle.penup()
+    n_turtle.color(colors[i])
+    n_turtle.goto(x=-230, y=y_pos[i])
+    turtles.append(n_turtle)
+
+
+if user_bet:
+    race_mode = True
+
+while race_mode:
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                screen.clear()
+                print(f'You win! {winning_color} colored wins the race!')
+            else:
+                screen.clear()
+                print(f'You lose.. {winning_color} colored wins the race.')
+            race_mode = False
+        r_distance = randint(0, 10)
+        turtle.forward(r_distance)
 
 screen.exitonclick()
